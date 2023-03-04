0e8619f7d17b64f9ba9e746f7e7920d797d20bf5
vifeng <virgvm@gmail.com>
2023-03-04 12:15:25 +0100

    changed time to check soil every 2 days

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ basic.forever(function () {
     moisture = pins.analogReadPin(AnalogPin.P1)
     if (moisture <= 400) {
         music.playSoundEffect(music.builtinSoundEffect(soundExpression.sad), SoundExpressionPlayMode.InBackground)
-        images.iconImage(IconNames.No).showImage(0)
+        images.iconImage(IconNames.Sad).showImage(0)
     } else {
-        images.iconImage(IconNames.Heart).showImage(0)
+        images.iconImage(IconNames.Happy).showImage(0)
     }
-    basic.pause(12 * (60*60*60*60))
+    basic.pause(300000)
 })
